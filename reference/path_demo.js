21540115e68442b96af164a49faa35a1b3caab20
mickey <mickey.a.warner@gmail.com>
2019-07-17 12:32:06 -0600

    examples for path module

---
+++ reference/path_demo.js
@@ -0,0 +1,19 @@
+// `path` is a core module included with node
+const path = require('path');
+
+// Base file name
+console.log(path.basename(__filename));
+
+// directory name
+console.log(path.dirname(__filename));
+
+// file extension
+console.log(path.extname(__filename));
+
+// create path object
+console.log(path.parse(__filename));
+console.log(path.parse(__filename).base);
+console.log(path.parse(__filename).dir);
+
+// concatenate paths
+console.log(path.join(__dirname, 'TEST', 'HELLO.html'));
